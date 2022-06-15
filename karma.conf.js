901a626a1666fac509f748640509884d126efa41
Ali Hakimi <ali.hakimi.njf@gmail.com>
2022-06-15 15:15:12 +0430

    fix: change project version

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fintranet'),
+      dir: require('path').join(__dirname, './coverage/prj'),
       subdir: '.',
       reporters: [
         { type: 'html' },
